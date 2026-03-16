ca4dc91446e9d74a8a8cd5cbe06979c39ac61861
luizhenriquesousad1-creator <luizhenriquesousad1@gmail.com>
2026-03-16 12:47:20 -0300

    Add sales data to dados_vendas.py

--- projetos/analisador_vendas/dados_vendas.py
+++ projetos/analisador_vendas/dados_vendas.py
@@ -1,1 +1,7 @@
 
+vendas = [
+{"produto":"teclado","preco":100,"quantidade":3},
+{"produto":"mouse","preco":50,"quantidade":5},
+{"produto":"monitor","preco":900,"quantidade":1},
+{"produto":"teclado","preco":100,"quantidade":2}
+]
