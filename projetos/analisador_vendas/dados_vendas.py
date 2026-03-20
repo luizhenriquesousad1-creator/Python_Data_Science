ba7dadb3250e5fecca1cc830f58177a70e1a3e3f
luizhenriquesousad1-creator <luizhenriquesousad1@gmail.com>
2026-03-19 21:25:38 -0300

    Delete projetos/analisador_vendas/dados_vendas.py
    remove dados de texto, pois usaremos apenas dados em arquivos .csv

--- projetos/analisador_vendas/dados_vendas.py
+++
@@ -1,7 +0,0 @@
-
-vendas = [
-{"produto":"teclado","preco":100,"quantidade":3},
-{"produto":"mouse","preco":50,"quantidade":5},
-{"produto":"monitor","preco":900,"quantidade":1},
-{"produto":"teclado","preco":100,"quantidade":2}
-]
